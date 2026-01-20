9baf7ea0fa9ac1b1849fc1c88ecf25c5b19e91a1
Cyanoxide <cyanoxide@gmail.com>
2026-01-20 14:26:28 +0000

    Add default wallpaper as background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import TaskBar from "./components/TaskBar/TaskBar"
+import { useState } from "react";
 
 function App() {
+  const [backgroundImage, setBackgroundImage] = useState("/wallpaper__default.jpg");
+
   return (
     <>
+      <img src={backgroundImage} width="100%" height="100%" className="fixed inset-0 object-cover object-center h-full" />
       <TaskBar />
     </>
   )
