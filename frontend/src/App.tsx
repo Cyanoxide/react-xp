bdd72739d2f940ca7abf65b69b6f6c0ee8ddd584
Cyanoxide <cyanoxide@gmail.com>
2026-01-20 14:43:39 +0000

    Convert Wallpaper to component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
 import { Provider } from "./context/provider";
-import { useState } from "react";
 import TaskBar from "./components/TaskBar/TaskBar"
+import Wallpaper from "./components/Wallpaper/Wallpaper"
 import './App.css';
 
 function App() {
-  const [backgroundImage, setBackgroundImage] = useState("/wallpaper__default.jpg");
-
   return (
     <Provider>
-      <img src={backgroundImage} width="100%" height="100%" className="fixed inset-0 object-cover object-center h-full" />
+      <Wallpaper />
       <TaskBar />
     </Provider>
   )
