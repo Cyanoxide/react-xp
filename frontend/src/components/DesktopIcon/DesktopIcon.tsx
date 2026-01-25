132aee87221852e73f742b0f37b5d63b8b780a48
Cyanoxide <cyanoxide@gmail.com>
2026-01-25 16:57:07 +0000

    Add movable desktop icons

--- frontend/src/components/DesktopIcon/DesktopIcon.tsx
+++ frontend/src/components/DesktopIcon/DesktopIcon.tsx
@@ -3,6 +3,7 @@ import type { currentWindow } from "../../context/types";
 import { useContext } from "../../context/context";
 import { generateUniqueId } from "../../utils/general";
 import { useState, useRef } from "react";
+import { throttle } from "../../utils/general";
 
 type DesktopIconProps = Omit<currentWindow, "id"> & {
     iconLarge?: string;
@@ -11,7 +12,31 @@ type DesktopIconProps = Omit<currentWindow, "id"> & {
 const DesktopIcon: React.FC<DesktopIconProps> = ({ title, iconLarge, icon, content }) => {
     const { currentWindows, dispatch } = useContext();
     const [isSelected, setIsSelected] = useState(false);
+    const [position, setPosition] = useState<number[]>([]);
     const desktopIcon = useRef<HTMLDivElement | null>(null);
+
+    const onPointerDown = (event: React.PointerEvent<HTMLDivElement>) => {
+        const desktopIconRect = desktopIcon.current?.getBoundingClientRect();
+        if (!desktopIconRect) return;
+
+        const xOffset = event.clientX - desktopIconRect.left;
+        const yOffset = event.clientY - desktopIconRect.top;
+        setIsSelected(true);
+
+        const onMouseMove = (event: MouseEvent) => {
+            setPosition([event.clientX - xOffset, event.clientY - yOffset]);
+            document.body.style.userSelect = "none";
+        }
+        const throttledMouseMove = throttle(onMouseMove, 50);
+
+        const onMouseUp = () => {
+            window.removeEventListener("mousemove", throttledMouseMove);
+            window.removeEventListener("mouseup", onMouseUp);
+            document.body.style.userSelect = "";
+        }
+        window.addEventListener("mousemove", throttledMouseMove);
+        window.addEventListener("mouseup", onMouseUp);
+    }
 
     const onClickHandler = () => {
         setIsSelected(true);
@@ -42,8 +67,8 @@ const DesktopIcon: React.FC<DesktopIconProps> = ({ title, iconLarge, icon, conte
     }
 
     return (
-        <div ref={desktopIcon} className={styles.desktopIcon} data-selected={isSelected} onClick={onClickHandler} onDoubleClick={onDoubleClickHandler}>
-            <span><img src={iconLarge || icon} width="50" height="50" /></span>
+        <div ref={desktopIcon} className={styles.desktopIcon} data-selected={isSelected} onClick={onClickHandler} onPointerDown={(e) => onPointerDown(e)} onDoubleClick={onDoubleClickHandler} style={{ left: position[0], top: position[1] }}>
+            <span><img src={iconLarge || icon} width="50" height="50" draggable={false}/></span>
             <h4>{title}</h4>
         </div>
     );
