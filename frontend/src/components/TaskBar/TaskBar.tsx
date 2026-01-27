331136c23be42efe5989bc414e0a96feac71b7b2
Cyanoxide <cyanoxide@gmail.com>
2026-01-27 00:55:38 +0000

    Deactive active window on open startmenu

--- frontend/src/components/TaskBar/TaskBar.tsx
+++ frontend/src/components/TaskBar/TaskBar.tsx
@@ -7,7 +7,7 @@ import StartMenu from "../StartMenu/StartMenu";
 const TaskBar = () => {
     const { currentTime, currentWindows, isStartVisible, dispatch } = useContext();
     const [systemTrayIconDismissed, setSystemTrayIconDismissed] = useState(false);
-    const startButton = useRef<HTMLButtonElement|null>(null);
+    const startButton = useRef<HTMLButtonElement | null>(null);
 
     //Todo: Add more accurate clock that updates in sync with system clock
     useEffect(() => {
@@ -46,17 +46,23 @@ const TaskBar = () => {
 
         setSystemTrayIconDismissed(false);
     }
-    
-    const statButtonClickHandler = () => {
-        dispatch({ type: "SET_IS_START_VISIBLE", payload: (isStartVisible) ? false : true});
+
+    const startButtonClickHandler = () => {
+        dispatch({ type: "SET_IS_START_VISIBLE", payload: (isStartVisible) ? false : true });
+
+        const updatedCurrentWindows = [...currentWindows];
+        const activeWindow = currentWindows.find(item => item.active === true);
+        if (activeWindow) activeWindow.active = false;
+
+        dispatch({ type: "SET_CURRENT_WINDOWS", payload: updatedCurrentWindows });
     }
 
     const formattedTime = currentTime.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
 
     return (
         <div className={`${styles.taskBar} flex justify-between`} data-label="taskbar">
-            <button ref={startButton} className={`${styles.startButton}`} onClick={statButtonClickHandler} data-selected={isStartVisible}>Start</button>
-            {isStartVisible && <StartMenu startButton={startButton}/>}
+            <button ref={startButton} className={`${styles.startButton}`} onClick={startButtonClickHandler} data-selected={isStartVisible}>Start</button>
+            {isStartVisible && <StartMenu startButton={startButton} />}
             <ul className={`${styles.windows} flex items-center justify-start w-full`}>
                 {currentWindows.map((currentWindow, index) => (
                     <li key={index} onClick={(e) => windowTabClickHandler(e)} data-label="taskBarWindowTab" data-active={currentWindow.active} data-window-id={currentWindow.id}>
