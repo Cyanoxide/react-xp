aeea3e0a5f5db13b4b31b0d651116a2562984189
Cyanoxide <cyanoxide@gmail.com>
2026-02-01 11:58:22 +0000

    Add FileExplorer primary app menu

--- frontend/src/components/Applications/FileExplorer/FileExplorer.tsx
+++ frontend/src/components/Applications/FileExplorer/FileExplorer.tsx
@@ -1,13 +1,57 @@
-// import styles from "./FileExplorer.module.scss";
+import styles from "./FileExplorer.module.scss";
 import WindowMenu from "../../WindowMenu/WindowMenu";
 
 const FileExplorer = () => {
-    // const onClickHandler = () => {
-
-    // }
 
     return (
-        <WindowMenu menuItems={["File", "Edit", "View", "Favorites", "Tools", "Help"]} hasWindowsLogo={true} />
+        <>
+            <WindowMenu menuItems={["File", "Edit", "View", "Favorites", "Tools", "Help"]} hasWindowsLogo={true} />
+            <section className={`${styles.appMenu} flex`}>
+                <div className="flex shrink-0">
+                    <button className="flex items-center m-0.5">
+                        <img className="mr-2" src="/icon__back.png" width="20" height="20"/>
+                        <h4>Back</h4>
+                        <span className="h-full"><span className={styles.dropdown}>▼</span></span>
+                    </button>
+                    <button className="flex items-center m-0.5">
+                        <img src="/icon__forward.png" width="20" height="20"/>
+                        <h4 className="hidden">Forward</h4>
+                        <span className="h-full"><span className={styles.dropdown}>▼</span></span>
+                    </button>
+                    <button className="flex items-center m-0.5">
+                        <img src="/icon__up.png" width="20" height="20"/>
+                        <h4 className="hidden">Up</h4>
+                    </button>
+                </div>
+                <div className="flex shrink-0">
+                    <button className="flex items-center m-0.5">
+                        <img className="mr-2" src="/icon__search--large.png" width="20" height="20"/>
+                        <h4>Search</h4>
+                    </button>
+                    <button className="flex items-center m-0.5">
+                        <img className="mr-2" src="/icon__folders.png" width="20" height="20"/>
+                        <h4>Folders</h4>
+                    </button>
+                </div>
+                <div className="flex shrink-0">
+                    <button className="flex items-center m-0.5" data-label="views">
+                        <img src="/icon__views.png" width="20" height="20" />
+                        <h4 className="hidden">Views</h4>
+                        <span className="h-full"><span className={styles.dropdown}>▼</span></span>
+                    </button>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <span>Address</span>
+                    <input type="text" />
+                    <div>
+                        <img />
+                        <span>Go</span>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
 
--- frontend/src/components/WindowMenu/WindowMenu.tsx
+++ frontend/src/components/WindowMenu/WindowMenu.tsx
@@ -10,14 +10,14 @@ const WindowMenu = ({ menuItems = [], hasWindowsLogo = false, isMinified = false
 
 
     return (
-        <div className={`${styles.windowMenu} flex ${hasWindowsLogo ? "justify-between" : "justify-start"}`} data-minified={isMinified}>
+        <section className={`${styles.windowMenu} flex ${hasWindowsLogo ? "justify-between" : "justify-start"}`} data-minified={isMinified}>
             <div className="relative overflow-hidden w-full">
                 <ul className="flex mx-1">
                     {menuItems.map((item, index) => <li key={index} className="display-block my-1 px-2.5 py-1"><button>{item}</button></li>)}
                 </ul>
             </div>
             {hasWindowsLogo && <img src="icon__windows_logo.png" height="100%" width="40" />}
-        </div>
+        </section>
     );
 };
 
