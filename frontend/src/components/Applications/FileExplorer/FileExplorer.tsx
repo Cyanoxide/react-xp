750384060c97e53f89e9c89985efe0b7a5661fd3
Cyanoxide <cyanoxide@gmail.com>
2026-02-02 12:02:36 +0000

    Fix bg accent z-index conflict

--- frontend/src/components/Applications/FileExplorer/FileExplorer.tsx
+++ frontend/src/components/Applications/FileExplorer/FileExplorer.tsx
@@ -50,59 +50,61 @@ const FileExplorer = ({ appId }: Record<string, string>) => {
 
     return (
         <>
-            <WindowMenu menuItems={["File", "Edit", "View", "Favorites", "Tools", "Help"]} hasWindowsLogo={true} />
-            <section className={`${styles.appMenu} relative`}>
-                <div className="flex absolute">
-                    <div className="flex shrink-0">
-                        <button className="flex items-center m-0.5">
-                            <img className="mr-2" src="/icon__back.png" width="20" height="20" />
-                            <h4>Back</h4>
-                            <span className="h-full"><span className={styles.dropdown}>▼</span></span>
-                        </button>
-                        <button className="flex items-center m-0.5">
-                            <img src="/icon__forward.png" width="20" height="20" />
-                            <h4 className="hidden">Forward</h4>
-                            <span className="h-full"><span className={styles.dropdown}>▼</span></span>
-                        </button>
-                        <button className="flex items-center m-0.5">
-                            <img src="/icon__up.png" width="20" height="20" />
-                            <h4 className="hidden">Up</h4>
-                        </button>
+            <div className={styles.menusContainer}>
+                <WindowMenu menuItems={["File", "Edit", "View", "Favorites", "Tools", "Help"]} hasWindowsLogo={true} />
+                <section className={`${styles.appMenu} relative`}>
+                    <div className="flex absolute">
+                        <div className="flex shrink-0">
+                            <button className="flex items-center m-0.5">
+                                <img className="mr-2" src="/icon__back.png" width="20" height="20" />
+                                <h4>Back</h4>
+                                <span className="h-full"><span className={styles.dropdown}>▼</span></span>
+                            </button>
+                            <button className="flex items-center m-0.5">
+                                <img src="/icon__forward.png" width="20" height="20" />
+                                <h4 className="hidden">Forward</h4>
+                                <span className="h-full"><span className={styles.dropdown}>▼</span></span>
+                            </button>
+                            <button className="flex items-center m-0.5">
+                                <img src="/icon__up.png" width="20" height="20" />
+                                <h4 className="hidden">Up</h4>
+                            </button>
+                        </div>
+                        <div className="flex shrink-0">
+                            <button className="flex items-center m-0.5">
+                                <img className="mr-2" src="/icon__search--large.png" width="20" height="20" />
+                                <h4>Search</h4>
+                            </button>
+                            <button className="flex items-center m-0.5">
+                                <img className="mr-2" src="/icon__folders.png" width="20" height="20" />
+                                <h4>Folders</h4>
+                            </button>
+                        </div>
+                        <div className="flex shrink-0">
+                            <button className="flex items-center m-0.5" data-label="views">
+                                <img src="/icon__views.png" width="20" height="20" />
+                                <h4 className="hidden">Views</h4>
+                                <span className="h-full"><span className={styles.dropdown}>▼</span></span>
+                            </button>
+                        </div>
                     </div>
-                    <div className="flex shrink-0">
-                        <button className="flex items-center m-0.5">
-                            <img className="mr-2" src="/icon__search--large.png" width="20" height="20" />
-                            <h4>Search</h4>
-                        </button>
-                        <button className="flex items-center m-0.5">
-                            <img className="mr-2" src="/icon__folders.png" width="20" height="20" />
-                            <h4>Folders</h4>
-                        </button>
-                    </div>
-                    <div className="flex shrink-0">
-                        <button className="flex items-center m-0.5" data-label="views">
-                            <img src="/icon__views.png" width="20" height="20" />
-                            <h4 className="hidden">Views</h4>
-                            <span className="h-full"><span className={styles.dropdown}>▼</span></span>
-                        </button>
-                    </div>
-                </div>
-            </section>
-            <section className={`${styles.navMenu} relative`}>
-                <div className="w-full h-full flex items-center absolute px-3">
-                    <span className={`${styles.navLabel} mr-1`}>Address</span>
+                </section>
+                <section className={`${styles.navMenu} relative`}>
+                    <div className="w-full h-full flex items-center absolute px-3">
+                        <span className={`${styles.navLabel} mr-1`}>Address</span>
 
-                    <div className={`${styles.navBar} flex mx-1 h-full`}>
-                        <img src={appData.icon || appData.iconLarge} className="mx-1" width="14" height="14" />
-                        <input ref={inputField} className={`${styles.navBar} h-full`} type="text" defaultValue={appData.title} onKeyDown={keyDownHandler} />
-                        <button className={styles.dropDown}>Submit</button>
+                        <div className={`${styles.navBar} flex mx-1 h-full`}>
+                            <img src={appData.icon || appData.iconLarge} className="mx-1" width="14" height="14" />
+                            <input ref={inputField} className={`${styles.navBar} h-full`} type="text" defaultValue={appData.title} onKeyDown={keyDownHandler} />
+                            <button className={styles.dropDown}>Submit</button>
+                        </div>
+                        <button className={`${styles.goButton} flex items-center`} onClick={updateWindow}>
+                            <img src="/icon__go.png" className="mr-1.5" width="19" height="19" />
+                            <span>Go</span>
+                        </button>
                     </div>
-                    <button className={`${styles.goButton} flex items-center`} onClick={updateWindow}>
-                        <img src="/icon__go.png" className="mr-1.5" width="19" height="19" />
-                        <span>Go</span>
-                    </button>
-                </div>
-            </section>
+                </section>
+            </div>
             <main className={`${styles.mainContent} h-full flex overflow-auto`} data-bg-accent={bgAccent}>
                 <aside className={`${styles.sidebar} h-full`}>
                     <CollapseBox title="File & Folder Tasks">
