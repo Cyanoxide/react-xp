0309e35e4f4ada8e684c7e2cfa36537e1d5ca289
Cyanoxide <cyanoxide@gmail.com>
2026-02-03 22:18:25 +0000

    Add back/forward/stop/refresh functionality to Internet Explorer

--- frontend/src/components/Applications/InternetExplorer/InternetExplorer.tsx
+++ frontend/src/components/Applications/InternetExplorer/InternetExplorer.tsx
@@ -2,76 +2,107 @@ import styles from "./InternetExplorer.module.scss";
 import WindowMenu from "../../WindowMenu/WindowMenu";
 import applicationsJSON from "../../../data/applications.json";
 import type { Application } from "../../../context/types";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { getBaseDomain, sameBaseDomain } from "../../../utils/general";
+import { getCurrentWindow } from "../../../utils/general";
+import { useContext } from "../../../context/context";
 
 
 const Applications = applicationsJSON as unknown as Record<string, Application>;
 
 const InternetExplorer = ({ appId }: Record<string, string>) => {
-    // const [isBackDisabled, setIsBackDisabled] = useState(true);
-    // const [isForwardDisabled, setIsForwardDisabled] = useState(true);
+    const { currentWindows, dispatch } = useContext();
+    const [isBackDisabled, setIsBackDisabled] = useState(true);
+    const [isForwardDisabled, setIsForwardDisabled] = useState(true);
+    const HOMEPAGE = "https://www.jamiepates.com";
 
-    // useEffect(() => {
-    //     const {currentWindow} = getCurrentWindow(currentWindows);
-    //     if (!currentWindow) return;
+    useEffect(() => {
+        const { currentWindow } = getCurrentWindow(currentWindows);
+        if (!currentWindow) return;
 
-    //     if (currentWindow.history) setIsBackDisabled(currentWindow.history.length === 0);
-    //     if (currentWindow.forward) setIsForwardDisabled(currentWindow.forward.length === 0);
-    // }, [currentWindows])
+        if (currentWindow.history) setIsBackDisabled(currentWindow.history.length === 0);
+        if (currentWindow.forward) setIsForwardDisabled(currentWindow.forward.length === 0);
+    }, [currentWindows]);
 
-    const inputField = useRef<HTMLInputElement | null>(null);
+    const inputFieldRef = useRef<HTMLInputElement | null>(null);
+    const inputField = inputFieldRef.current;
+    const currentUrl = (inputField) ? inputField.value : null;
+
     const appData = Applications[appId];
 
     const keyDownHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === "Enter") {
-            updateIframe();
+            goClickHandler();
         }
     }
 
-    // const backClickHandler = () => {
-    //     const {currentWindow, updatedCurrentWindows} = getCurrentWindow(currentWindows);
-    //     if (!currentWindow || !currentWindow.history) return;
+    const backClickHandler = () => {
+        const { currentWindow, updatedCurrentWindows } = getCurrentWindow(currentWindows);
+        if (!currentWindow || !currentWindow.history || !inputField) return;
 
-    //     if (currentWindow.forward) currentWindow.forward.push(currentWindow.appId);
+        if (currentWindow.forward && currentUrl) currentWindow.forward.push(currentUrl);
+        const previousUrl = currentWindow.history.pop() || "";
 
-    //     const previousWindowId = currentWindow.history.pop() || "";
+        inputField.value = previousUrl;
+        updateIframe();
 
-    //     currentWindow.appId = previousWindowId;
-    //     dispatch({ type: "SET_CURRENT_WINDOWS", payload: updatedCurrentWindows });
-    // }
+        dispatch({ type: "SET_CURRENT_WINDOWS", payload: updatedCurrentWindows });
+    }
 
-    // const forwardClickHandler = () => {
-    //     const {currentWindow, updatedCurrentWindows} = getCurrentWindow(currentWindows);
-    //     if (!currentWindow || !currentWindow.forward) return;
+    const forwardClickHandler = () => {
+        const { currentWindow, updatedCurrentWindows } = getCurrentWindow(currentWindows);
+        if (!currentWindow || !currentWindow.forward || !inputField) return;
 
-    //     if (currentWindow.history) currentWindow.history.push(currentWindow.appId);
+        if (currentWindow.history && currentUrl) currentWindow.history.push(currentUrl);
+        const previousUrl = currentWindow.forward.pop() || "";
 
-    //     const previousWindowId = currentWindow.forward.pop() || "";
+        inputField.value = previousUrl;
+        updateIframe();
 
-    //     currentWindow.appId = previousWindowId;
-    //     dispatch({ type: "SET_CURRENT_WINDOWS", payload: updatedCurrentWindows });
-    // }
+        dispatch({ type: "SET_CURRENT_WINDOWS", payload: updatedCurrentWindows });
+    }
 
     const iframeRef = useRef<HTMLIFrameElement | null>(null);
+    const iframe = iframeRef.current;
 
     const updateIframe = () => {
-        const inputRef = inputField.current;
-        const value = (!inputRef?.value.startsWith("http")) ? `https://${inputRef?.value}` : inputRef?.value;
+        const value = (!inputField?.value.startsWith("http")) ? `https://${inputField?.value}` : inputField?.value;
         const wayBackUrl = "https://web.archive.org/web/20030612074004if_/"
         const url = (!value.includes(getBaseDomain())) ? `/proxy.php?url=${wayBackUrl}${value}` : value;
 
-        if (inputRef && iframeRef.current) {
+        if (iframe) {
             if (sameBaseDomain(value)) {
-                iframeRef.current.setAttribute("sandbox", "allow-same-origin allow-forms");
-                iframeRef.current.setAttribute("referrerPolicy", "no-referrer");
+                iframe.setAttribute("sandbox", "allow-same-origin allow-forms");
+                iframe.setAttribute("referrerPolicy", "no-referrer");
             } else {
-                iframeRef.current.removeAttribute("sandbox");
-                iframeRef.current.removeAttribute("referrerPolicy");
+                iframe.removeAttribute("sandbox");
+                iframe.removeAttribute("referrerPolicy");
             }
         }
 
-        if (inputRef && iframeRef.current) iframeRef.current.setAttribute("src", url);
+        if (inputField && iframe) iframe.setAttribute("src", url);
+    }
+
+    const goClickHandler = () => {
+        const { currentWindow, updatedCurrentWindows } = getCurrentWindow(currentWindows);
+        if (currentWindow && currentWindow.history && currentUrl) {
+            currentWindow.history.push(currentUrl);
+        }
+        dispatch({ type: "SET_CURRENT_WINDOWS", payload: updatedCurrentWindows });
+        updateIframe();
+    }
+
+    const stopClickHandler = () => {
+        if (iframe) iframe.setAttribute("src", "about:blank");
+    }
+
+    const refreshClickHandler = () => {
+        updateIframe();
+    }
+
+    const homeClickHandler = () => {
+        if (inputField) inputField.value = HOMEPAGE;
+        if (iframe) iframe.setAttribute("src", HOMEPAGE);
     }
 
     return (
@@ -81,25 +112,25 @@ const InternetExplorer = ({ appId }: Record<string, string>) => {
                 <section className={`${styles.appMenu} relative`}>
                     <div className="flex absolute">
                         <div className="flex shrink-0">
-                            <button className="flex items-center m-0.5 cursor-not-allowed">
+                            <button className="flex items-center m-0.5" onClick={backClickHandler} disabled={isBackDisabled}>
                                 <img className="mr-2" src="/icon__back.png" width="20" height="20" />
                                 <h4>Back</h4>
                                 <span className="h-full"><span className={styles.dropdown}>▼</span></span>
                             </button>
-                            <button className="flex items-center m-0.5 cursor-not-allowed">
+                            <button className="flex items-center m-0.5" onClick={forwardClickHandler} disabled={isForwardDisabled}>
                                 <img src="/icon__forward.png" width="20" height="20" />
                                 <h4 className="hidden">Forward</h4>
                                 <span className="h-full"><span className={styles.dropdown}>▼</span></span>
                             </button>
-                            <button className="flex items-center m-0.5 cursor-not-allowed">
+                            <button className="flex items-center m-0.5" onClick={stopClickHandler}>
                                 <img src="/icon__stop--large.png" width="20" height="20" />
                                 <h4 className="hidden">Stop</h4>
                             </button>
-                            <button className="flex items-center m-0.5 cursor-not-allowed">
+                            <button className="flex items-center m-0.5" onClick={refreshClickHandler}>
                                 <img src="/icon__refresh--large.png" width="20" height="20" />
                                 <h4 className="hidden">Refresh</h4>
                             </button>
-                            <button className="flex items-center m-0.5 cursor-not-allowed">
+                            <button className="flex items-center m-0.5" onClick={homeClickHandler}>
                                 <img src="/icon__home--large.png" width="20" height="20" />
                                 <h4 className="hidden">Home</h4>
                             </button>
@@ -136,7 +167,7 @@ const InternetExplorer = ({ appId }: Record<string, string>) => {
 
                         <div className={`${styles.navBar} flex mx-1 h-full`}>
                             <img src={appData.icon || appData.iconLarge} className="mx-1" width="14" height="14" />
-                            <input ref={inputField} className={`${styles.navBar} h-full`} type="text" defaultValue="https://www.jamiepates.com" onKeyDown={keyDownHandler} />
+                            <input ref={inputFieldRef} className={`${styles.navBar} h-full`} type="text" defaultValue={HOMEPAGE} onKeyDown={keyDownHandler} />
                             <button className={styles.dropDown}>Submit</button>
                         </div>
                         <button className={`${styles.goButton} flex items-center`} onClick={() => updateIframe()}>
@@ -147,7 +178,7 @@ const InternetExplorer = ({ appId }: Record<string, string>) => {
                 </section>
             </div>
             <main className={`${styles.mainContent} h-full flex overflow-auto`}>
-                <iframe ref={iframeRef} src="https://www.jamiepates.com" width="100%" height="100%" />
+                <iframe ref={iframeRef} src={HOMEPAGE} width="100%" height="100%" />
             </main >
         </>
     );
